89230f78d74e38af0fa1ee0a35e7d6a22f1aa02f
Anton Yefremov <anton.yefremov@gmail.com>
2013-01-01 20:41:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# jQuery Plugin Template
+
+This is my own choice of jQuery plugin pattern which mainly consists of collection method, its initialization, defaults management and public API definition. Static method and custom pseudo selector definition also included.
+
+Since I am a big fan of modular JavaScript I had added UMD (Universal Module Definition) version of jQuery plugin template that works with AMD (Asynchronous Module Definition) and browser globals. You can read more about UMD patterns on its repository [page](https://github.com/umdjs/umd).
+
+Replace any instance of word `plugin` with your actual plugin `name`.
+
+## License
+
+Copyright (c) 2012 Anton Yefremov  
+Free to use and abuse under the MIT license.  
+[http://opensource.org/licenses/MIT](http://opensource.org/licenses/MIT)
---
+++ jquery.plugin.js
@@ -0,0 +1,89 @@
+/*
+ * jQuery Plugin - v0.1.0
+ * https://github.com/yefremov/jquery-plugin
+ *
+ * Copyright (c) 2012 Anton Yefremov
+ * Free to use and abuse under the MIT license.
+ * http://opensource.org/licenses/MIT
+ */
+
+(function ( window, $, undefined ) {
+  'use strict';
+
+  // Collection method
+  $.fn.plugin = function ( settings ) {
+
+    // Plugin class definition
+    function Plugin( element, options ) {
+      var settings;
+
+      // Initialization method
+      function initialize( options ) {
+        settings = options;
+      }
+
+      // Destroy method
+      function destroy() {
+        element.removeData( settings.namespace );
+      }
+
+      // Public API (access via .data method)
+      $.extend( this, {
+        'reinitialize': function ( options ) {
+          destroy();
+          initialize( options );
+        }
+      , 'destroy': function () {
+          destroy();
+        }
+      });
+
+      // Initialize plugin
+      initialize( options );
+    }
+
+    // Customize settings
+    settings = $.extend( {}, $.fn.plugin.defaults, settings, $.fn.plugin.privates );
+
+    // Maintain chainability
+    return this.each(function () {
+      var element = $( this )
+        , api = element.data( settings.namespace );
+
+      if ( api ) {
+        // Reinitialize if already initialized
+        api.reinitialize( settings );
+      } else {
+        // Create new instance and reveal public API
+        element.data( settings.namespace, new Plugin(element, settings) );
+      }
+    });
+  };
+
+  // Collection privates (modify on your own risk)
+  $.fn.plugin.privates = {
+    'namespace': 'jquery-plugin'
+  };
+
+  // Collection defaults
+  $.fn.plugin.defaults = {
+    // Default settings goes here
+  };
+
+  // Method to override defaults
+  $.fn.plugin.setDefaults = function ( settings ) {
+    $.extend( $.fn.plugin.defaults, settings );
+  };
+
+  // Static method
+  $.plugin = function () {
+    // Singleton instance definition
+  };
+
+  // Custom pseudo selector
+  $.expr[':'].plugin = $.expr.createPseudo(function () {
+    return function ( element ) {
+      return element;
+    };
+  });
+}) ( this, jQuery );
